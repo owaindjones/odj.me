eb4e18139146ee23f927190bce3fb05a0f718d0f
Owain Jones <owanes@noc.ac.uk>
2026-09-14 22:04:31 +0100

    publish via github action

--- gen.sh
+++ gen.sh
@@ -3,43 +3,43 @@ set -x
 
 cd `dirname $0`
 
-# Clear out build
-rm -vrf build
+# Clear out public
+rm -vrf public
 
 # Copy static files
-mkdir -p build
-cp -vrf ./static build/static
+mkdir -p public
+cp -vrf ./static public/static
 
 # Generate pages
 find pages/ -name '*.html' | while read page; do
     output=`realpath --relative-to $PWD/pages $page`
     output_dir=`dirname $output`
-    mkdir -p build/$output_dir
+    mkdir -p public/$output_dir
     extra_headers=''
     extra_footers=''
     if [[ "$page" = *"blog"* ]]; then
          extra_headers="$extra_headers components/blog-header.html"
 	 extra_footers="$extra_footers components/blog-footer.html"
     fi
-    cat components/header.html $extra_headers $page $extra_footers components/footer.html > build/$output
+    cat components/header.html $extra_headers $page $extra_footers components/footer.html > public/$output
 done
 
 # Build the blog index
-mkdir -p build/blog
-cat components/header.html > build/blog/index.html
-cat components/blog-header.html >> build/blog/index.html
+mkdir -p public/blog
+cat components/header.html > public/blog/index.html
+cat components/blog-header.html >> public/blog/index.html
 page=$(find pages/blog/ -name '*.html' | sort -hr | head -n1)
 if [[ "$page" != "" ]]; then
     base=`basename $page .html`
-    awk '/<!--summary-->/,/<!--\/summary-->/' $page >> build/blog/index.html
-    echo "<p><a href=\"/blog/${base}\">Read more...</a></p>" >> build/blog/index.html
-    echo "<div class=\"hr\"></div><strong class=\"magenta\">Articles</strong><br />" >> build/blog/index.html
-    echo "<ul>" >> build/blog/index.html
+    awk '/<!--summary-->/,/<!--\/summary-->/' $page >> public/blog/index.html
+    echo "<p><a href=\"/blog/${base}\">Read more...</a></p>" >> public/blog/index.html
+    echo "<div class=\"hr\"></div><strong class=\"magenta\">Articles</strong><br />" >> public/blog/index.html
+    echo "<ul>" >> public/blog/index.html
     find pages/blog/ -name '*.html' | sort -hr | while read page; do
         base=`basename $page .html`
-        echo "<li><a href=\"/blog/${base}\">${base}</a></li>" >> build/blog/index.html
+        echo "<li><a href=\"/blog/${base}\">${base}</a></li>" >> public/blog/index.html
     done
-    echo "</ul>" >> build/blog/index.html
+    echo "</ul>" >> public/blog/index.html
 fi
-cat components/blog-footer.html >> build/blog/index.html
-cat components/footer.html >> build/blog/index.html
+cat components/blog-footer.html >> public/blog/index.html
+cat components/footer.html >> public/blog/index.html
